c49f44ea0e947b1b866e1f99b92c306400371074
Manaila Robert <147810577+ManailaRobert@users.noreply.github.com>
2025-05-26 14:21:19 +0300

    Add project files.

---
+++ DataAdder_SoftwareDevelopmentProductivityAPP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAdder_SoftwareDevelopmentProductivityAPP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
